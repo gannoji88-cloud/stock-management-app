8dab09e6d0517cb412b2bd682585842347ae258f
スヌスムムリク <gannoji88@hotmail.com>
2026-05-17 16:08:14 +0900

    UI update

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# ① ページ設定（←一番上）
+# ① ページ設定
 st.set_page_config(page_title="在庫管理", layout="wide")
 
-st.title("在庫管理ダッシュボード")
+st.title("COBOL × Python 在庫管理システム")
+
+# 説明
+st.write("COBOLで在庫更新を行い、Pythonで分析・可視化しています。")
+
+# GitHubリンク
+st.markdown(
+    "[GitHubはこちら](https://github.com/gannoji88-cloud/stock-management-app.git)"
+)
+
+st.subheader("在庫データ分析ダッシュボード")
+
+st.markdown("""
+### 使用技術
+- COBOL
+- Python
+- pandas
+- matplotlib
+- Streamlit
+""")
+
+st.divider()
 
 # データ読み込み
 df = pd.read_fwf(
@@ -22,7 +43,7 @@ col1.metric("商品数", len(df))
 col2.metric("在庫合計", df["stock"].sum())
 col3.metric("最小在庫", df["stock"].min())
 
-# ⑤ フィルタ（先に置くと自然）
+# ⑤ フィルタ
 threshold = st.slider("在庫しきい値", 0, 200, 80)
 
 st.subheader("在庫一覧")
